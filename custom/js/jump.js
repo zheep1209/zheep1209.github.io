f8d35407e15f6a2be93f812dd18503c2fd48c062
zheep <663574252@qq.com>
2025-04-28 19:30:11 +0800

    Site updated: 2025-04-28 19:30:10

--- custom/js/jump.js
+++ custom/js/jump.js
@@ -1,11 +1,1 @@
-// 主站和 GitHub Pages 的 URL
-const mainSite = "www.zheep.top";
-const localSite = "localhost";
-
-// 判断当前页面的主机名是否是主站或本地站
-if (window.location.hostname === mainSite || window.location.hostname === localSite) {
-  console.log("好！很有精神！！！");
-} else {
-  // 跳转到主站
-  window.location.href = `https://${mainSite}`;
-}
+let mainSite="www.zheep.top",localSite="localhost";window.location.hostname===mainSite||window.location.hostname===localSite?console.log("好！很有精神！！！"):window.location.href="https://"+mainSite;
